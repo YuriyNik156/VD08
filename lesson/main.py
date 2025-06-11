60c93ebed78d93d35962f294f60985363d6167e7
YuriyNik156 <yuriy.nik156@gmail.com>
2025-06-11 20:19:47 +0300

    update main.py and index.html

--- lesson/main.py
+++ lesson/main.py
@@ -6,16 +6,24 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     weather = None
+    news = None
     if request.method == "POST":
-        city = request.form[("city")]
+        city = request.form.get("city")
         weather = get_weather(city)
-    return render_template("index.html", weather=weather)
+        news = get_news()
+    return render_template("index.html", weather=weather, news=news)
 
 def get_weather(city):
-    api_key = "your api key"
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    api_key = "13e2e91ca5585170bfc25294bde29a16"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=ru"
     response = requests.get(url)
     return response.json()
+
+def get_news():
+    api_key = "01570c09ab3a4e3fbc4082e3921d48f1"
+    url = f"https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey={api_key}"
+    response = requests.get(url)
+    return response.json().get("articles", [])
 
 if __name__ == "__main__":
     app.run(debug=True)
